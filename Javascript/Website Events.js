95693b54fc61b78b0dab9176d75a4b46b5b738d7
tripp <tripp@DESKTOP-5NTIASM>
2017-02-09 23:43:38 -0800

    Adding everything

---
+++ Javascript/Website Events.js
@@ -0,0 +1,18 @@
+﻿document.getElementById("SummaryButton").addEventListener("click", function () {
+    window.location = '/HTML/About Me.html'
+});
+document.getElementById("ResumeButton").addEventListener("click", function () {
+    window.location = '/HTML/Resume.html'
+});
+document.getElementById("VlogButton").addEventListener("click", function () {
+    window.location = '/HTML/Vlog Page.html'
+});
+document.getElementById("TwitchButton").addEventListener("click", function () {
+    window.location = '/HTML/Twitch Page.html'
+});
+document.getElementById("boxOne").addEventListener("click", function () {
+    window.location = '/HTML/About Me.html'
+});
+document.getElementById("boxTwo").addEventListener("click", function () {
+    window.location = '/HTML/Projects Page.html'
+});
